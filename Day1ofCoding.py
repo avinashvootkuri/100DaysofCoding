858a2a62175d720d015e4a9658cede08b78a5088
Avinash Vootkuri <avinashchandrav@gmail.com>
2022-05-27 02:07:28 -0400

    Dsy1 of Coding

---
+++ Day1ofCoding.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator.")
+city = input("What's name of the city you grew up in?\n")
+# print(city)
+pet_name = input("What's your pet's name?\n")
+# print(pet_name)
+print("Your band name could be " +city+ " " + pet_name)
