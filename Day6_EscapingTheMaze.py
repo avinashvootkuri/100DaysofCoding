1e744ad94be4d2e2622173f900f89818e688eb10
Avinash Shruthi <avinashshruthi@Avinashs-MBP.ht.home>
2022-05-30 21:06:56 -0400

    Day 6 - Escaping the maze

---
+++ Day6_EscapingTheMaze.py
@@ -0,0 +1,3 @@
+### Code for day 6 is in the below link:
+### It is solved using reeborg.ca, and the code is in the repl
+###https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
